91cf309f454013cbedf74c91bf3961796a3a0aa3
CORPORATIF\agagnon <agagnon@cegepba.qc.ca>
2018-09-28 10:24:53 -0400

    Cours11 Debut

---
+++ Cours11/Assets/Script/Oscillator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+    [SerializeField] Vector3 movementVector = new Vector3(10f,10f,10f);
+    [SerializeField] float period = 2f;
+
+    [Range(0,1)]
+    [SerializeField]
+    float movementFactor;
+    Vector3 startingPos;
+
+	void Start () {
+        startingPos = transform.position;
+	}
+
+	void Update () {
+        if(period <= Mathf.Epsilon) {
+            return;
+        }
+      
+	}
+}
